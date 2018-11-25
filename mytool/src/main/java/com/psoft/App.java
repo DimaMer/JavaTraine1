a49ec3a113892e02bdd686beaa97ab8d2e0b778c
DimaMer <dimm1simm1@gmail.com>
2018-11-26 01:48:45 +0200

    url

--- mytool/src/main/java/com/psoft/App.java
+++ mytool/src/main/java/com/psoft/App.java
@@ -1,14 +1,41 @@
 package com.psoft;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+import java.net.*;
+import java.io.*;
+
+public class App {
+
+
+
+    private static final String fName = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
+    public static void main(String[] args)  {
+
+        try {
+
+                    StringBuilder parsedContentFromUrl = new StringBuilder();
+            URL sURL = new URL(fName);
+            URLConnection myURLCon = sURL.openConnection();
+                     myURLCon.connect();
+                       BufferedInputStream in = new BufferedInputStream(myURLCon.getInputStream());
+
+            int ch;
+            while ((ch = in.read()) != -1) {
+                parsedContentFromUrl.append((char) ch);
+
+            }
+            System.out.println("Full Source JSON string:");
+            System.out.println(parsedContentFromUrl);
+
+            in.close();
+        }
+        catch (MalformedURLException e) {
+                       System.out.println("--MalformedURLException");
+        }
+        catch (IOException e) {
+                       e.printStackTrace();
+            System.out.println("--IOException");
+        }
+
     }
 }
